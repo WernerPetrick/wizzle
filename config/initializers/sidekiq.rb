342751759e4053889d9d4b8fee2c4bd335eb060d
Werner Petrick <werpet@gmail.com>
2025-07-12 21:27:28 +0200

    Updated sidekiq initialiser

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,19 @@
 require 'sidekiq'
 require 'sidekiq-cron'
 
-Sidekiq::Cron::Job.create(
-  name: 'Birthday Reminder',
-  cron: '0 6 * * *', # every day at 6am
-  class: 'BirthdayReminderJob'
-)
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV.fetch("REDIS_URL", "redis://localhost:6379") }
+
+  schedule = {
+    'birthday_reminder' => {
+      'cron' => '0 6 * * *', # every day at 6am
+      'class' => 'BirthdayReminderJob'
+    }
+  }
+
+  Sidekiq::Cron::Job.load_from_hash(schedule)
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV.fetch("REDIS_URL", "redis://localhost:6379") }
+end
