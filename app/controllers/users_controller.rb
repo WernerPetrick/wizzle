54eebd5762a865e3e2d2bd8e76d0a5d4bbd2f750
Werner Petrick <werpet@gmail.com>
2025-07-13 14:27:15 +0200

    Added new action to skip_before_action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_login
-  skip_before_action :require_login, only: [:create]
+  skip_before_action :require_login, only: [:new, :create]
 
   def create
     super do |user|
