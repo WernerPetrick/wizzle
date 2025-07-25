338126a562f728b9210c9819e940a7314883fdb5
Werner Petrick <werpet@gmail.com>
2025-07-25 18:07:41 +0200

    Fixed schema

--- db/schema.rb
+++ db/schema.rb
@@ -55,17 +55,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_24_181345) do
     t.index ["user_id"], name: "index_blog_posts_on_user_id"
   end
 
-  create_table "blog_posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.boolean "published"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.datetime "published_at"
-    t.index ["user_id"], name: "index_blog_posts_on_user_id"
-  end
-
   create_table "friendships", id: :integer, force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "friend_id", null: false
