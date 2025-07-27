262ec402db48c20a5644eddc28fc5da0b501e47c
Werner Petrick <werpet@gmail.com>
2025-07-27 21:48:10 +0200

    Updated extensions for redcarpet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,19 @@ module ApplicationHelper
 
   def markdown(text)
     renderer = Redcarpet::Render::HTML.new(filter_html: true, hard_wrap: true)
-    markdown = Redcarpet::Markdown.new(renderer, extensions = {})
+    extensions = {
+      autolink: true,
+      fenced_code_blocks: true,
+      tables: true,
+      strikethrough: true,
+      underline: true,
+      highlight: true,
+      quote: true,
+      footnotes: true
+    }
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
     markdown.render(text || "")
   end
+
 
 end
